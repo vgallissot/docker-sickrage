353160439683de154abbf9dbf32f83c6bfc0cba1
Vincent Gallissot <vgallissot.externe@m6.fr>
2017-02-21 21:37:40 +0100

    enhance build script + add missing python package
    * force 'latest' tag on newly built docker image
    * always use date tag on built image
    * add missing python dependancy mandatory for app to build

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm --no-cache --pull -t timhaak/sickrage .
+D=$(date +%Y%m%d%H%M)
+docker build --rm --no-cache --pull -t sickrage:${D} .
+docker tag sickrage:${D} sickrage:latest
